a04ec48773d7e9551f8bfc316bb44c2975e9d5a6
akalanka <akalanka@echonlabs.com>
2023-10-09 09:05:06 +0530

    update user done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -47,9 +47,24 @@ const currentUserController = async (req, res) => {
   res.json({ message: "Current User", data: req.user });
 };
 
+const updateUserControllersFunction = async (req, res) => {
+  try {
+    const user = await userService.updateUser(req.params.id, req.body);
+    res.status(200).json({
+      status: true,
+      message: "user updated successfully",
+      data: user,
+    });
+  } catch (err) {
+    console.error("An error occurred", error.message);
+    res.status(500).json({ status: false, message: error.message });
+  }
+};
+
 module.exports = {
   registerController,
   loginUserController,
   currentUserController,
   getAllUserController,
+  updateUserControllersFunction,
 };
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 module.exports = router;
-const userController = require('../controllers/user.controller');
-const validateToken = require('../middleware/validateToken');
+const userController = require("../controllers/user.controller");
+const validateToken = require("../middleware/validateToken");
 
-router.get("/",userController.getAllUserController);
-// router.get("/:id",userController.getUserControllersFunction);
-router.post("/register",userController.registerController);
-router.post("/login",userController.loginUserController);
-router.get("/current", validateToken.validateToken , userController.currentUserController);
-// router.patch("/:id",userController.updateUserControllersFunction);
+router.get("/", userController.getAllUserController);
+router.post("/register", userController.registerController);
+router.post("/login", userController.loginUserController);
+router.get(
+  "/current",
+  validateToken.validateToken,
+  userController.currentUserController
+);
+router.patch("/:id", userController.updateUserControllersFunction);
 // router.delete("/:id",userController.deleteUserControllersFunction);
