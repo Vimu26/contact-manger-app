b055e77ab0a3541b26efaabecbb61f7b8ce6356c
akalanka <akalanka@echonlabs.com>
2023-10-14 15:38:57 +0530

    express barerer token added and fixes done

--- services/token.service.js
+++ services/token.service.js
@@ -5,7 +5,7 @@ module.exports.generateToken = async (existingUser) => {
     {
       sub: existingUser._id, //subject Claim
       iss: existingUser.user_name, // Issuer claim
-      exp: Math.floor(Date.now() / 1000) + 30 * 60, // Expiration time in seconds (30 minutes)
+      exp: Math.floor(Date.now() / 1000) + 80 * 60, // Expiration time in seconds (30 minutes)
       aud: "System Login", // Audience claim
       iat: Math.floor(Date.now() / 1000), // Issued At claim
       existingUser: {
