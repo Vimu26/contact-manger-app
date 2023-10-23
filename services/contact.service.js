972877409f24e3a5374ea7280c0e63d628f19838
akalanka <akalanka@echonlabs.com>
2023-10-23 11:46:57 +0530

    removed unwanted code add compare password service

--- services/contact.service.js
+++ services/contact.service.js
@@ -1,19 +1,6 @@
 const model = require("../models/contact.model");
 
-// module.exports.getAllContacts = (req) => {
-//   return model
-//     .find({ user_id: req.user.userExist.id })
-//     .then((contacts) => {
-//       if (contacts.length === 0) {
-//         console.log("No contacts found");
-//       }
-//       return contacts;
-//     })
-//     .catch((error) => {
-//       throw error;
-//     });
-// };
-module.exports.getAllContacts = async (req) => {
+xports.getAllContacts = async (req) => {
   const contacts = await model.find({ user_id: req.user.sub });
   return contacts;
 };
@@ -24,60 +11,46 @@ module.exports.getSingleContact = async (id) => {
 };
 
 module.exports.createContact = async (details) => {
-  try {
-    const data = new model({
-      user_id: details.user.sub,
-      name: details.body.name,
-      contact_number: details.body.contact_number,
-      email: details.body.email,
-    });
-    await data.save();
-    return data;
-  } catch (error) {
-    throw error;
-  }
+  const data = new model({
+    user_id: details.user.sub,
+    name: details.body.name,
+    contact_number: details.body.contact_number,
+    email: details.body.email,
+  });
+  await data.save();
+  return data;
 };
 
 module.exports.updateContact = async (userId, contactBody, contactId) => {
-  try {
-    const contact = await model.findById(contactId);
-    if (!contact) {
-      return {
-        error: "Contact not found",
-      };
-    }
-    if (userId !== contact.user_id.toString()) {
-      return {
-        error: "user don't have permission to edit this contact",
-      };
-    }
-    const data = await model.findByIdAndUpdate(contactId, contactBody, {
-      new: true,
-    });
-    return data;
-  } catch (error) {
-    console.error("An error occurred during user update:", error);
-    throw error;
+  const contact = await model.findById(contactId);
+  if (!contact) {
+    return {
+      error: "Contact not found",
+    };
   }
+  if (userId !== contact.user_id.toString()) {
+    return {
+      error: "user don't have permission to edit this contact",
+    };
+  }
+  const data = await model.findByIdAndUpdate(contactId, contactBody, {
+    new: true,
+  });
+  return data;
 };
 
 module.exports.deleteContact = async (req) => {
-  try {
-    const contact = await model.findById(req.params.id);
-    if (!contact) {
-      return {
-        error: "Contact not found",
-      };
-    }
-    if (req.user.sub !== contact.user_id.toString()) {
-      return {
-        error: "user don't have permission to delete this contact",
-      };
-    }
-    const data = await model.findByIdAndDelete(req.params.id);
-    return data;
-  } catch (error) {
-    console.error("An error occurred during user Delete:", error);
-    throw error;
+  const contact = await model.findById(req.params.id);
+  if (!contact) {
+    return {
+      error: "Contact not found",
+    };
   }
+  if (req.user.sub !== contact.user_id.toString()) {
+    return {
+      error: "user don't have permission to delete this contact",
+    };
+  }
+  const data = await model.findByIdAndDelete(req.params.id);
+  return data;
 };
